527ef0ae017f0de2cbb3a939c56332bf293bcdb9
taijarals <taijara@outlook.com.br>
2026-05-14 13:40:52 -0300

    secrets gemini

--- config.py
+++ config.py
@@ -6,6 +6,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-GEMINI_API_KEY = os.getenv(
-    "GEMINI_API_KEY"
-)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- services/gemini_service.py
+++ services/gemini_service.py
@@ -13,6 +13,7 @@ genai.configure(
 )
 
 
+
 model = genai.GenerativeModel(
     "gemini-1.5-flash"
 )
