fe8b70b58526ab093db0d6a65a4bda6c7790a04e
taijarals <taijara@outlook.com.br>
2026-05-14 09:34:17 -0300

    pages/resolver_questoes.py

---
+++ pages/dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def tela_dashboard():
+
+    st.title("📊 Dashboard")
+
+    st.write("Aqui ficará o dashboard.")
---
+++ pages/resolver_questoes.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def tela_resolver():
+
+    st.title("📝 Resolver Questões")
+
+    st.write("Aqui ficará a resolução.")
