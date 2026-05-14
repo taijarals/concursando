e5f46e13928f2cc739a92b9b68efb4b5cd799580
taijarals <taijara@outlook.com.br>
2026-05-14 09:28:03 -0300

    app login

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 import streamlit as st
 
+from auth import login, register
+
 st.set_page_config(
-    page_title="Concurso AI",
+    page_title="lorem ipsum",
     layout="wide"
 )
 
-st.title("📚 Concurso AI")
+if "user" not in st.session_state:
 
-st.write("Sistema inteligente de questões")
+    aba1, aba2 = st.tabs([
+        "Login",
+        "Cadastro"
+    ])
+
+    with aba1:
+        login()
+
+    with aba2:
+        register()
+
+else:
+
+    st.sidebar.success(
+        f"Logado como: "
+        f"{st.session_state['user'].email}"
+    )
+
+    if st.sidebar.button("Logout"):
+
+        del st.session_state["user"]
+
+        st.rerun()
+
+    st.title("📚 Concurso AI")
+
+    st.write("Sistema carregado!")
