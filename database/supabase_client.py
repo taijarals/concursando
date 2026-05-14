238effedfc9c4efd1adbfb6209743ba15a696e07
taijarals <taijara@outlook.com.br>
2026-05-14 10:15:27 -0300

    .replace("/rest/v1/","").rstrip("/")

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL").replace("/rest/v1/","")
+url = os.getenv("SUPABASE_URL").replace("/rest/v1/","").rstrip("/")
 key = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(url, key)
